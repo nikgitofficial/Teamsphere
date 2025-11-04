72206aa7374d0c68d72a385b1e8c814f3bce4d73
nikgitofficial <nickforjobacc@gmail.com>
2025-11-05 02:30:36 +0800

    Extend access token expiration from 10s to 15m

--- backend/utils/tokenUtils.js
+++ backend/utils/tokenUtils.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const createAccessToken = (user) => {
-  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "10s" });
+  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "15m" });
 };
 
 export const createRefreshToken = (user) => {
